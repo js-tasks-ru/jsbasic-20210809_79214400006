3e6b595070e900ba2351fe5fbb48799355db06d8
Dmitriy Maslennikov <79214400006@ya.ru>
2021-08-15 02:25:48 +0300

    Lesson 1 - 5/5

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -8,10 +8,41 @@ function print(text) {
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
- */
-function isValid(name) {
-  // ваш код...
+ 
+ function isValid(name) {
+  if  (name === null)  {
+    return false;
+  }
+  else { 
+  let x = name.split('')
+  if (x.length >= 4) {
+  for (let i = 0; i <= x.length; i++){
+  if (x[i] === ' ') {
+  return false;
 }
+else { 
+  return true; 
+}}}}}*/
+
+function isValid(name) {
+  let a = true;
+  if  (name === null)  {
+    return false;
+  }
+    else { 
+      let x = name.split('')
+      if (x.length < 4) {
+      return false; 
+      }
+        else {
+          for (let i = 0; i <= x.length; i++){
+            if (x[i] === ' ') {
+              a = false
+            }
+}}}
+return a; 
+}
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
