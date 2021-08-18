311e629ffd6c1b6ece52e9b9dbcd850361172584
Dmitriy Maslennikov <79214400006@ya.ru>
2021-08-18 23:04:48 +0300

    Lesson 2 - 3/3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let result = 0;
+ for (let key in salaries) {
+   if (typeof(salaries[key]) === 'number' 
+   && Number.isFinite(salaries[key]) // Является ли число конечным?
+   && !Number.isNaN(salaries[key])){ // (Не) Является ли число 'NaN'
+     result = result + salaries[key]
+   }
+ }
+   return ( result );
+ }
