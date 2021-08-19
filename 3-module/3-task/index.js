ef428527e42f42c468e1e994a1515e98d5b9ecbf
Dmitriy Maslennikov <79214400006@ya.ru>
2021-08-20 01:54:35 +0300

    Lesson 3 - 5/5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,22 @@
 function camelize(str) {
-  // ваш код...
+  let separator = str.split('-');
+  console.log(separator);
+  let arr = [];
+  let word;
+  if (separator[0] === '') {
+  separator.shift();
+  for (let i = 0; i < separator.length; i++){
+    word = separator[i];
+    word = word[0].toUpperCase() + word.slice(1);
+    arr.push(word);
+  }}
+  else {
+    arr.push(separator.shift());
+    for (let i = 0; i < separator.length; i++){
+    word = separator[i];
+    word = word[0].toUpperCase() + word.slice(1);
+    arr.push(word);
+    }
+  }
+  return arr.join('')
 }
