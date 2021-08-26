b01c4db1ec0b9a9c3fae354609ae5c54f0a7368e
Dmitriy Maslennikov <79214400006@ya.ru>
2021-08-26 17:57:19 +0300

    Lesson 4 - 3/3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  let resultMap = friends.map(user => `${user.firstName} ${user.lastName}`);
+  
+  for(let i = 0; i < resultMap.length; i++){
+    let str = '<li>'+ resultMap[i] + '</li>';
+    ul.insertAdjacentHTML('beforeEnd', str);
+  }
+  return ul;
 }
