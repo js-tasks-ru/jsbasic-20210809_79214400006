c82e6d307bbb64c0ff061b5ed061efdd777c3ded
Dmitriy Maslennikov <79214400006@ya.ru>
2021-09-01 01:10:41 +0300

    Lesson 5 - 3/3

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button')
+  let text = document.querySelector('#text')
+
+  let hidden = () => { text.toggleAttribute('hidden') }
+  button.addEventListener('click', hidden)
+
 }
